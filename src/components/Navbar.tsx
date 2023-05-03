f14e8b04e35be101ccbcaea4c4f8c312e82fe1bb
Oscar A. Peña C <subtitular@gmail.com>
2023-05-02 21:41:57 -0500

    Algo de imagen corporativa

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+//import { logo } from "./logopoli_w.png";
 
 interface IProps{}
 interface IState{}
@@ -9,6 +10,13 @@ let Navbar:React.FC<IProps>=()=>{
         <>
             <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
                 <div className="container">
+                <img
+          src="/logopoli_w.png"
+          width="91"
+          height="62"
+          className="d-inline-block align-top"
+          alt="Logo"
+        />{" "}
                 <Link to={'/'} className="navbar-brand">Home</Link>
                 <Link to={'/about'} className="navbar-brand">Nosotros</Link>
                     
